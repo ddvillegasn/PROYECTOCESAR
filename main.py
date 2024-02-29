c0fe68de1c5c6fc8530fe059ec38c74468044b51
Cesar Villegas <161660581+ddvillegasn@users.noreply.github.com>
2024-02-28 22:19:58 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from API import obtener_datos_covid
-from UI import mostrar_datos
+from UI.mostrar_datos import mostrar_datos
+from API.obtener_datos_covid import obtener_datos_covid
 
 def main():
     limite_registros = input("Digite el número de registros que desea consultar:")
@@ -7,5 +7,6 @@ def main():
     data_frame = obtener_datos_covid(limite_registros, nombre_departamento.upper())
     mostrar_datos(data_frame)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
+
